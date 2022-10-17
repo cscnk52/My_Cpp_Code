29e9d76704382398929ad45e3e265b1bbbe7fd60
cscnk52 <8492338+cscnk52@user.noreply.gitee.com>
2022-10-17 21:53:10 +0800

    20221016d

--- oj/HRBUST/20221016/d.cpp
+++ oj/HRBUST/20221016/d.cpp
@@ -4,7 +4,13 @@
 #include <cstring>
 #include <numeric>
 using namespace std;
-void insert(int x1, int x2, int y1, int y2, int p)
+
+int num[1000][1000];
+int rect[4][1000];
+int sum[1000][1000];
+int diff[1000][1000];
+
+void insert(int x1, int y1, int x2, int y2, int p)
 {
     diff[x1][y1] += p;
     diff[x2 + 1][y1] -= p;
@@ -12,14 +18,9 @@ void insert(int x1, int x2, int y1, int y2, int p)
     diff[x2 + 1][y2 + 1] += p;
 }
 
-int num[1000][1000];
-int rect[4][1000];
-int sum[1000][1000];
-int diff[1000][1000];
-
 int main()
 {
-    int n, m, q, x1, x2, y1, y2, p;
+    int n, m, q, p;
 
     memset(num, 0, sizeof(num));
     memset(rect, 0, sizeof(rect));
@@ -29,20 +30,28 @@ int main()
     scanf("%d%d", &n, &m);
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++)
-            scanf("%d", &num[i][j]);
+            scanf("%d", &sum[i][j]);
 
     scanf("%d", &q);
     for (int i = 1; i <= q; i++)
     {
-        scanf("%d%d%d%d")
+        scanf("%d%d%d%d%d", &rect[0][i], &rect[1][i], &rect[2][i], &rect[3][i], &p);
+        insert(rect[0][i], rect[1][i], rect[2][i], rect[3][i], p);
     }
 
     for (int i = 1; i <= n; i++)
-    {
         for (int j = 1; j <= m; j++)
-        {
-            sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + num[i][j];
-        }
-    }
+            diff[i][j] = diff[i][j] + diff[i][j - 1] + diff[i - 1][j] - diff[i - 1][j - 1];
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++)
+            sum[i][j] += sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + diff[i][j];
+
+    int maxx = 0;
+    for (int i = 1; i <= q; i++)
+        maxx = max(maxx, sum[rect[2][i]][rect[3][i]] + sum[rect[0][i] - 1][rect[1][i] - 1] - sum[rect[0][i] - 1][rect[3][i]] - sum[rect[2][i]][rect[1][i] - 1]);
+
+    printf("%d\n", maxx);
+
     return 0;
 }
