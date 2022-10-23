ddd7917222fc0638f92fbfc7ae1c4cb9351a4f9b
cscnk52 <8492338+cscnk52@user.noreply.gitee.com>
2022-10-23 20:35:32 +0800

    fix:20221021实验

--- oj/HRBUST/22021021/d.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    double f;
-    scanf("%lf", &f);
-    printf("%.2f\n", 5.0 / 9 * (f - 32));
-    return 0;
-}
--- oj/HRBUST/22021021/e.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    double a;
-    scanf("%lf", &a);
-    printf("%f\n", 30 *3.6* a);
-}
