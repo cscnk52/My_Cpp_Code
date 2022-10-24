5a6a06e0892e497cfb7a2e0c0d5da3d0f6c338b2
cscnk52 <8492338+cscnk52@user.noreply.gitee.com>
2022-10-24 22:01:40 +0800

    fix:week7d

--- oj/HRBUST/week7/d.cpp
+++ oj/HRBUST/week7/d.cpp
@@ -1,18 +1,17 @@
 #include <iostream>
 using namespace std;
-long long gcd(long long a,long long b)
+long long gcd(long long a, long long b)
 {
-    if(b>a)
+    if (b > a)
         return gcd(b, a);
-    if(b==0)
+    if (b == 0)
         return a;
-    a -= b;
-    return gcd(a, b);
+    return gcd(a, a % b);
 }
 int main()
 {
     long long a, b;
-    while(~scanf("%lld%lld", &a, &b))
+    while (~scanf("%lld%lld", &a, &b))
     {
         printf("%lld %lld\n", gcd(a, b), a * b / gcd(a, b));
     }
