c90e7430e02000b900e6902a8f3d7e350560d09b
cscnk52 <8492338+cscnk52@user.noreply.gitee.com>
2022-10-27 08:58:34 +0800

    fix:20221023d尾递归优化

--- oj/HRBUST/20221023/d.cpp
+++ oj/HRBUST/20221023/d.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
-long long fib[91] = {0, 1, 1};
+long long fib(int n, int ret = 0, int tmp = 1)
+{
+    if (n == 0) return ret;
+    else return fib(n - 1, ret + tmp, ret);
+}
 int main()
 {
     int num;
-    for (int i = 3; i <= 90;i++)
+    while (~scanf("%d", &num))
     {
-        fib[i] = fib[i - 1] + fib[i - 2];
+        printf("%lld\n", fib(num));
     }
-        while (~scanf("%d", &num))
-        {
-            printf("%lld\n", fib[num]);
-        }
     return 0;
 }
