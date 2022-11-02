0d311df2ce4563386dc4807a8ffebfb5817353db
cscnk52 <8492338+cscnk52@user.noreply.gitee.com>
2022-11-02 22:35:25 +0800

    code_make_me_laugh

---
+++ function/Code_me_laugh1.cpp
@@ -0,0 +1,314 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    while (~scanf("%d", &a))
+    {
+        switch (a)
+        {
+        case 0:
+            cout << "zero\n";
+            break;
+        case 1:
+            cout << "one\n";
+            break;
+        case 2:
+            cout << "two\n";
+            break;
+        case 3:
+            cout << "three\n";
+            break;
+        case 4:
+            cout << "four\n";
+            break;
+        case 5:
+            cout << "five\n";
+            break;
+        case 6:
+            cout << "six\n";
+            break;
+        case 7:
+            cout << "seven\n";
+            break;
+        case 8:
+            cout << "eight\n";
+            break;
+        case 9:
+            cout << "nine\n";
+            break;
+        case 10:
+            cout << "ten\n";
+            break;
+        case 11:
+            cout << "eleven\n";
+            break;
+        case 12:
+            cout << "twelve\n";
+            break;
+        case 13:
+            cout << "thirteen\n";
+            break;
+        case 14:
+            cout << "fourteen\n";
+            break;
+        case 15:
+            cout << "fifteen\n";
+            break;
+        case 16:
+            cout << "sixteen\n";
+            break;
+        case 17:
+            cout << "seventeen\n";
+            break;
+        case 18:
+            cout << "eighteen\n";
+            break;
+        case 19:
+            cout << "nineteen\n";
+            break;
+        case 20:
+            cout << "twenty\n";
+            break;
+        case 21:
+            cout << "twenty-one\n";
+            break;
+        case 22:
+            cout << "twenty-two\n";
+            break;
+        case 23:
+            cout << "twenty-three\n";
+            break;
+        case 24:
+            cout << "twenty-four\n";
+            break;
+        case 25:
+            cout << "twenty-five\n";
+            break;
+        case 26:
+            cout << "twenty-six\n";
+            break;
+        case 27:
+            cout << "one\n";
+            break;
+        case 28:
+            cout << "twenty-eight\n";
+            break;
+        case 29:
+            cout << "twenty-nine\n";
+            break;
+        case 30:
+            cout << "thirty\n";
+            break;
+        case 31:
+            cout << "thirty-one\n";
+            break;
+        case 32:
+            cout << "thirty-two\n";
+            break;
+        case 33:
+            cout << "thirty-three\n";
+            break;
+        case 34:
+            cout << "thirty-four\n";
+            break;
+        case 35:
+            cout << "thirty-five\n";
+            break;
+        case 36:
+            cout << "thirty-six\n";
+            break;
+        case 37:
+            cout << "thirty-seven\n";
+            break;
+        case 38:
+            cout << "thirty-eight\n";
+            break;
+        case 39:
+            cout << "thirty-nine\n";
+            break;
+        case 40:
+            cout << "forty\n";
+            break;
+        case 41:
+            cout << "forty-one\n";
+            break;
+        case 42:
+            cout << "forty-two\n";
+            break;
+        case 43:
+            cout << "forty-three\n";
+            break;
+        case 44:
+            cout << "forty-four\n";
+            break;
+        case 45:
+            cout << "forty-five\n";
+            break;
+        case 46:
+            cout << "forty-six\n";
+            break;
+        case 47:
+            cout << "forty-seven\n";
+            break;
+        case 48:
+            cout << "forty-eight\n";
+            break;
+        case 49:
+            cout << "forty-nine\n";
+            break;
+        case 50:
+            cout << "fifty\n";
+            break;
+        case 51:
+            cout << "fifty-one\n";
+            break;
+        case 52:
+            cout << "fifty-two\n";
+            break;
+        case 53:
+            cout << "fifty-three\n";
+            break;
+        case 54:
+            cout << "fifty-four\n";
+            break;
+        case 55:
+            cout << "fifty-five\n";
+            break;
+        case 56:
+            cout << "fifty-six\n";
+            break;
+        case 57:
+            cout << "fifty-seven\n";
+            break;
+        case 58:
+            cout << "fifty-eight\n";
+            break;
+        case 59:
+            cout << "fifty-nine\n";
+            break;
+        case 60:
+            cout << "sixty\n";
+            break;
+        case 61:
+            cout << "sixty-one\n";
+            break;
+        case 62:
+            cout << "sixty-two\n";
+            break;
+        case 63:
+            cout << "sixty-three\n";
+            break;
+        case 64:
+            cout << "sixty-four\n";
+            break;
+        case 65:
+            cout << "sixty-five\n";
+            break;
+        case 66:
+            cout << "sixty-six\n";
+            break;
+        case 67:
+            cout << "sixty-seven\n";
+            break;
+        case 68:
+            cout << "sixty-eight\n";
+            break;
+        case 69:
+            cout << "sixty-nine\n";
+            break;
+        case 70:
+            cout << "seventy\n";
+            break;
+        case 71:
+            cout << "seventy-one\n";
+            break;
+        case 72:
+            cout << "seventy-two\n";
+            break;
+        case 73:
+            cout << "seventy-three\n";
+            break;
+        case 74:
+            cout << "seventy-four\n";
+            break;
+        case 75:
+            cout << "seventy-five\n";
+            break;
+        case 76:
+            cout << "seventy-six\n";
+            break;
+        case 77:
+            cout << "seventy-seven\n";
+            break;
+        case 78:
+            cout << "seventy-eight\n";
+            break;
+        case 79:
+            cout << "seventy-nine\n";
+            break;
+        case 80:
+            cout << "eighty\n";
+            break;
+        case 81:
+            cout << "eighty-one\n";
+            break;
+        case 82:
+            cout << "seventy-two\n";
+            break;
+        case 83:
+            cout << "eighty-three\n";
+            break;
+        case 84:
+            cout << "eighty-four\n";
+            break;
+        case 85:
+            cout << "eighty-five\n";
+            break;
+        case 86:
+            cout << "eighty-six\n";
+            break;
+        case 87:
+            cout << "eighty-seven\n";
+            break;
+        case 88:
+            cout << "eighty-eight\n";
+            break;
+        case 89:
+            cout << "eighty-nine\n";
+            break;
+        case 90:
+            cout << "ninety\n";
+            break;
+        case 91:
+            cout << "ninety-one\n";
+            break;
+        case 92:
+            cout << "ninety-two\n";
+            break;
+        case 93:
+            cout << "ninety-three\n";
+            break;
+        case 94:
+            cout << "ninety-four\n";
+            break;
+        case 95:
+            cout << "ninety-five\n";
+            break;
+        case 96:
+            cout << "ninety-six\n";
+            break;
+        case 97:
+            cout << "ninety-seven\n";
+            break;
+        case 98:
+            cout << "ninety-eight\n";
+            break;
+        case 99:
+            cout << "ninety-nine\n";
+            break;
+        }
+    }
+    return 0;
+}
